3f8f6e687cf22559a11e30fabe5bf0fc872c7670
Joel H. <143587522+Joelh123@users.noreply.github.com>
2025-05-20 11:39:47 +0300

    5.7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,32 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [moreVisible, setMoreVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const showWhenVisible = { display: moreVisible ? 'none' : '' }
+  const hideWhenVisible = { display: moreVisible ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      <div style={showWhenVisible}>
+        {blog.title} {blog.author} <button onClick={() => setMoreVisible(!moreVisible)}>view</button>
+      </div>
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author} <button onClick={() => setMoreVisible(!moreVisible)}>hide</button>
+        <div>{blog.url}</div>
+        <div>{blog.likes} <button>like</button></div>
+        <div>{blog.author}</div>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
